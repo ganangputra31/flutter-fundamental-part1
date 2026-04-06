be9c7521ece2ab225d897d3409fc3525eedd776f
gnangputra31 <myinwork14@gmail.com>
2026-04-07 03:50:05 +0700

    Selection Widget

--- README.md
+++ README.md
@@ -11,3 +11,4 @@ A new Flutter project.
 ![Screenshot fab_widget](images/05.png)
 ![Screenshot scaffold_widget](images/06.png)
 ![Screenshot dialog_widget](images/07.png)
+![Screenshot  Selection Widget](images/08.png)
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/cupertino.dart'; // Jika menggunakan Cupertino style
-import 'basic_widgets/dialog_widget.dart'; // Pastikan import file dialog_widget.dart
 
 void main() {
   runApp(const MyApp());
@@ -12,61 +10,16 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.purple, // Tema aplikasi
-      ),
-      home: const MyHomePage(title: 'My Increment App'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text(widget.title)),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter', // Menampilkan angka counter
-              style: const TextStyle(
-                fontSize: 24,
-                fontWeight: FontWeight.bold,
-              ), // Gaya teks untuk angka
-            ),
-            const SizedBox(height: 50), // Jarak antara counter dan tombol FAB
-            DialogWidget(), // Memanggil widget dialog
-          ],
+      home: Scaffold(
+        appBar: AppBar(title: const Text("Contoh TextField")),
+        body: const TextField(
+          obscureText: false,
+          decoration: InputDecoration(
+            border: OutlineInputBorder(),
+            labelText: 'Nama',
+          ),
         ),
       ),
-      bottomNavigationBar: BottomAppBar(child: Container(height: 50.0)),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment Counter',
-        child: const Icon(Icons.add), // Ikon tambah
-      ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
     );
   }
 }
