bdac273ef3d9867f583240666aed7fee65c6aab2
gnangputra31 <myinwork14@gmail.com>
2026-04-07 01:20:03 +0700

    cupertino_widget

--- README.md
+++ README.md
@@ -7,3 +7,4 @@ A new Flutter project.
 ![Screenshot hello_world](images/01.png)
 ![Screenshot text_widget](images/02.png)
 ![Screenshot image_widget](images/03.png)
+![Screenshot cupertino_widget](images/04.png)
--- lib/basic_widgets/loading_cupertino.dart
+++ lib/basic_widgets/loading_cupertino.dart
@@ -1,2 +1,39 @@
-import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart'; // Pastikan cupertino diimpor
+import 'package:flutter/material.dart';
+
+class MyLoadingCupertino extends StatelessWidget {
+  const MyLoadingCupertino({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const CupertinoActivityIndicator(); // Menampilkan indikator loading Cupertino
+  }
+}
+
+class MyCupertinoWidget extends StatelessWidget {
+  const MyCupertinoWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          "Contoh Cupertino Widget",
+        ), // Menambahkan judul pada AppBar
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment:
+              MainAxisAlignment.center, // Memusatkan widget di tengah layar
+          children: <Widget>[
+            CupertinoButton(
+              child: const Text("Contoh button"),
+              onPressed: () {},
+            ),
+            const CupertinoActivityIndicator(), // Menampilkan indikator loading
+          ],
+        ),
+      ),
+    );
+  }
+}
